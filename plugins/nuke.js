99727ed250ec098dfd10f4944fa2f5edc72a0589
djejufidnenfhdn <kestomod055@gmail.com>
2026-02-24 18:14:57 +0100

    Update nuke.js

--- plugins/nuke.js
+++ plugins/nuke.js
@@ -12,7 +12,7 @@ let handler = async (m, { conn, participants, isBotAdmin }) => {
     try {
         let metadata = await conn.groupMetadata(m.chat);
         let oldName = metadata.subject;
-        let newName = `${oldName} | 𝐒𝐕𝐓 𝐁𝐘 #𝐌𝐀𝐋𝐓𝐑𝐀𝐓𝐓𝐀𝐑𝐄`;
+        let newName = `${oldName} | 𝐒𝐕𝐓 𝐁𝐘 𝐌𝐀𝐋𝐓𝐑𝐀𝐓𝐓𝐀𝐑𝐄`;
         await conn.groupUpdateSubject(m.chat, newName);
     } catch (e) {
         console.error('Errore cambio nome gruppo:', e);
