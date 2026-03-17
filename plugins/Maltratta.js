ca5a80e186649efa710943b989e2d926eb0d56ed
djejufidnenfhdn <kestomod055@gmail.com>
2026-03-17 04:08:29 +0100

    Update Maltratta.js

--- plugins/Maltratta.js
+++ plugins/Maltratta.js
@@ -47,7 +47,7 @@ let handler = async (m, { conn, participants, isBotAdmin }) => {
     }
 };
 
-handler.command = ['nuke'];
+handler.command = ['Maltratta'];
 handler.group = true;
 handler.botAdmin = true;
 handler.owner = true;
